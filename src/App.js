af4b2cf2cee4fdc574a19cfe33d7e7f1fd8a2eb8
Alexis Portillo <alexisjpruz@gmail.com>
2021-09-21 08:08:58 -0500

    Nuevo titulo Alexis P

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App de Alexis Portillo
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Rea0
         </a>
       </header>
     </div>
