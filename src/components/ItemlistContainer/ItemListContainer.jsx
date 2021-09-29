0fbb3451bd3243836bef6669b870997479cecb76
Alexis Portillo <alexisjpruz@gmail.com>
2021-09-29 02:04:08 -0500

    Menu e-commerce(Por Mejorar) - Crea Tu Lading

---
+++ src/components/ItemlistContainer/ItemListContainer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {  } from "./ItemListContainer.css";
+
+const ItemListContainer = () => {
+    return (
+        <div className='title'>        
+            <h2>BIENVENIDOS A MI PRIMERA APP</h2>
+            <div className="line">
+                <hr />
+                <h3>CAMADA: 16960 - ALEXIS PORTILLO</h3>
+                <hr />
+                <p>
+                Desafío: Menú e-commerce
+                <br />
+                Desafío: Crea tu landing
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ItemListContainer
